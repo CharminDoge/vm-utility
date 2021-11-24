abfe5215396612e5d6f777a38f73e776596e30b6
charmindoge <danielechianella@hotmail.it>
2021-11-24 15:51:32 +0100

    scrap mods to main

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
-#include "magic_packet.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
 
 int main(){
-    send_magic_packet();
+    if (send_magic_packet() == EXIT_SUCCESS){
+        printf("%Lf", main_send_ping("gooasdgle.it", 3));
+        if (main_send_ping("google.it", 3) == 0.0){
+            printf("bella raga\n");
+        }
+    }
+    else{
+        printf("sucks\n");
+    }
+
 }
